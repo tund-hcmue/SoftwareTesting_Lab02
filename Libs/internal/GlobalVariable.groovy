a5a71dab853397527436f6252ac441f87c62f5f9
tund-hcmue <tund.fit@gmail.com>
2020-09-30 19:53:28 +0700

    remote file gitinoge

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,42 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object username
+     
+    /**
+     * <p></p>
+     */
+    public static Object usernames
+     
+    /**
+     * <p></p>
+     */
+    public static Object usenamee
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            username = selectedVariables['username']
+            usernames = selectedVariables['usernames']
+            usenamee = selectedVariables['usenamee']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
